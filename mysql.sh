2e23e7d8d1e8393835ec493eb9be6543b27f4fa8
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-02-27 14:05:52 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
+###------------------------------------------------------------------------------------------------------------------------------
 ### untuk dump DB mysql .sql
 
 name=$(cat table.txt | awk -v a=$1 -v b=$2 'NR==a,NR==b')
-folder_dump=/home/asdf/database/dsp_new
+folder_dump=/home/server98.115/database/dsp_new
 
 echo $1 " " $2 >> log_dump.txt
 
